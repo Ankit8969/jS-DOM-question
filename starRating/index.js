e9ec4a2900f861cf231b0481db9ce34ac995581b
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2022-12-04 13:37:00 +0530

    Create index.js

---
+++ starRating/index.js
@@ -0,0 +1,20 @@
+// getting all the stars
+let stars = document.getElementsByTagName('a');
+let inPutBox = document.querySelector('input');
+
+function addingListenerToStar(){
+    for(let i=0 ;i<stars.length;i++){
+        stars[i].addEventListener("click", ()=>{
+            for (let j=0;j<stars.length;j++){
+                if (j<=i)
+                    stars[j].style.opacity = "100%";
+                else
+                    stars[j].style.opacity = "50%";
+            }
+            inPutBox.style.visibility = "visible";
+        })
+    }
+}
+
+// addingEvent Listener
+addingListenerToStar();
